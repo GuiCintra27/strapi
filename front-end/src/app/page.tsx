37f20683d4a5bf028730d57282e0c23b4e0c7c56
Cintra <gui7lk@gmail.com>
2024-07-01 15:32:43 -0300

    refact: types for strapi api data

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,21 +1,38 @@
 import { Button } from "@/components/ui/button";
+type HomeData = {
+  data: {
+    attributes: {
+      Title: string;
+      description: string;
+    };
+  };
+};
 
-async function getStrapiData(url: string) {
+async function getHomeData(
+  url: string
+): Promise<{ Title: string; description: string } | undefined> {
   const baseUrl = "http://localhost:1337";
   try {
     const response = await fetch(`${baseUrl}${url}`);
-    const data = await response.json();
-    return data;
+    const data: HomeData = await response.json();
+
+    if (!data) return undefined;
+
+    return {
+      Title: data.data.attributes.Title,
+      description: data.data.attributes.description,
+    };
   } catch (error) {
     console.error(error);
   }
 }
 
 export default async function Home() {
-  const data = await getStrapiData("/api/home-page");
-  console.log(data);
+  const data = await getHomeData("/api/home-page");
   return (
     <div>
+      <h1>{data?.Title}</h1>
+      <p>{data?.description}</p>
       <Button>Click me</Button>
     </div>
   );
